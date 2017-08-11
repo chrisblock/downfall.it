45f628fb06063195589eb0d57a78f9538bf7239e
djkrush <matthew_j_brock@hotmail.com>
2017-08-10 22:02:43 -0500

    Got downfall.wmv to open successfully in LibAvFormatWrapper

--- src/Downfall.It.Core/LibAvFormatWrapper.cpp
+++ src/Downfall.It.Core/LibAvFormatWrapper.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 
 #include "LibAvFormatWrapper.h"
+#include <iostream>
+#include <fstream>
 
 extern "C"
 {
@@ -11,6 +13,16 @@ extern "C"
 DownfallItCore::LibAvFormatWrapper::LibAvFormatWrapper()
 {
 	av_register_all();
+
+	auto input_format = av_find_input_format("asf");
+	auto output_format = av_guess_format(__nullptr, "output.mkv", __nullptr);
+
+	//auto context = avformat_alloc_context();
+
+	AVFormatContext *input = __nullptr;
+	auto result = avformat_open_input(&input, "E:\\Projects\\downfall.it\\resources\\videos\\downfall.wmv", input_format, __nullptr);
+
+	avformat_close_input(&input);
 }
 
 DownfallItCore::LibAvFormatWrapper::~LibAvFormatWrapper()
--- src/Downfall.It.Core/Merger.cpp
+++ src/Downfall.It.Core/Merger.cpp
@@ -1,10 +1,13 @@
 #include "stdafx.h"
 
 #include "Merger.h"
+#include "LibAvFormatWrapper.h"
 
 array<System::Byte> ^DownfallItCore::Merger::AddSubtitles(array<System::Byte> ^subtitleFileContents)
 {
 	auto str = Encoding::UTF8->GetString(subtitleFileContents);
+
+	LibAvFormatWrapper x;
 
 	return Encoding::UTF8->GetBytes(str);
 }
